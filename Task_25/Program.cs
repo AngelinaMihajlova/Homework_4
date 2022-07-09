f0ffa8adde9fe188a8666ce2f256c56a9931c551
AngelinaMihajlova <mihajlovaangelina@yandex.ru>
2022-07-09 14:53:09 +0300

    Создана новая папка Task_25

---
+++ Task_25/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Программа, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵);  2, 4 -> 16 */
+
+double usePow (int A, int B)
+{
+    double result = Math.Pow (A,B);
+    Console.WriteLine (result);
+    return result;
+}
+
+usePow (3, 5);
+Console.WriteLine ();
+usePow (2, 4);
