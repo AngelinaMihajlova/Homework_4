e4b2599d74bf4e07861cd87e68b2356f618d1e1c
AngelinaMihajlova <mihajlovaangelina@yandex.ru>
2022-07-09 15:59:08 +0300

    Добавлена новая папка Task_27

---
+++ Task_27/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Программа, которая принимает на вход число и выдаёт сумму цифр в числе
+452 -> 11; 82 -> 10; 9012 -> 12 */
+
+int SumNumbers (int N)
+{
+    int sum = 0;
+    while (N > 0)
+    {
+        sum = sum + N % 10;
+        N = N / 10;
+    }
+Console.WriteLine (sum);
+return sum;
+}
+
+SumNumbers (452);
+SumNumbers (82);
+SumNumbers (9012);
